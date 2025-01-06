d6b62c0ccc84dfb57cbd19e422ffdabdafa3752e
Leo <lanker013@gmail.com>
2025-01-06 13:28:53 +0000

    UPDATE About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>About Page</h1>
         <p>This is the About page!</p>
+        <p>We do so many cool things for the people of the shire that you would not even believe.</p>
     </div>
 </template>
 <script>
